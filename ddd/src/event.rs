5feed5398d21eac60bdece5d842a9cc1e09ab80e
Benjamin Kuen <info@benjamin-kuen.com>
2022-07-25 16:46:47 +0200

    Make domain events send and sync

--- ddd/src/event.rs
+++ ddd/src/event.rs
@@ -2,7 +2,7 @@ use std::fmt::Debug;
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-pub trait DomainEvent: Debug {
+pub trait DomainEvent: Debug + Send + Sync {
     fn id(&self) -> Uuid;
     fn timestamp(&self) -> DateTime<Utc>;
 }
