1be4f6145dfd02fd5804f9c260be24626d01c790
Benjamin Kuen <info@benjamin-kuen.com>
2022-07-26 19:28:10 +0200

    Update persistence model helper traits

--- ddd/src/core.rs
+++ ddd/src/core.rs
@@ -7,10 +7,6 @@ pub trait ToPersistence<T> {
 /// A helper trait to convert a persistence model or dao into a domain `T`
 pub trait ToDomain<T> {
     /// Converts itself into a domain `T`
-    ///
-    /// # Arguments
-    ///
-    /// * `persistence` - The persistence model or dao
-    fn to_domain(persistence: Self) -> T;
+    fn to_domain(&self) -> T;
 }
 
